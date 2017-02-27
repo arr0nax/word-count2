4c1adefee729758dce5c298435f1c9a4be39da03
Clayton Collins <claytonccollins@gmail.com>
2017-02-26 20:24:26 -0800

    rename things to proper conventions

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,5 +1,5 @@
 <?php
-    class Count {
+    class RepeatCounter {
         public $sentence;
         public $sentence_array;
         public $word;
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,11 +1,11 @@
 <?php
-    require_once 'src/Count.php';
+    require_once 'src/RepeatCounter.php';
 
     class SourceTest extends PHPUnit_Framework_TestCase
     {
         function test_parse_function() {
             $input = 'the quick brown brown fox';
-            $test_count = new Count($input);
+            $test_count = new RepeatCounter($input);
 
             $result = $test_count->parse_sentence();
 
@@ -15,7 +15,7 @@
         function test_count_function(){
             $input_sentence = 'the quick brown brown fox';
             $input_word = 'brown';
-            $test_count = new Count($input_sentence, $input_word);
+            $test_count = new RepeatCounter($input_sentence, $input_word);
 
             $test_count->parse_sentence();
             $result = $test_count->count_words();
@@ -26,7 +26,7 @@
         function test_ignore_special_characters() {
             $input_sentence = 'the qui<ck brown brown fox';
             $input_word = 'brown_';
-            $test_count = new Count($input_sentence, $input_word);
+            $test_count = new RepeatCounter($input_sentence, $input_word);
 
             $test_count->remove_special_chars();
             $test_count->parse_sentence();
@@ -38,7 +38,7 @@
         function test_remove_word_spaces() {
             $input_sentence = 'the qui<ck brown brown fox';
             $input_word = 'brown_ ';
-            $test_count = new Count($input_sentence, $input_word);
+            $test_count = new RepeatCounter($input_sentence, $input_word);
 
             $test_count->remove_special_chars();
             $test_count->remove_word_spaces();
@@ -51,7 +51,7 @@
         function test_no_matches() {
             $input_sentence = 'the qui<ck brown brown fox';
             $input_word = 'red';
-            $test_count = new Count($input_sentence, $input_word);
+            $test_count = new RepeatCounter($input_sentence, $input_word);
 
             $test_count->remove_special_chars();
             $test_count->remove_word_spaces();
@@ -65,7 +65,7 @@
         {
             $input_sentence = 'the qui<ck Brown browN fox';
             $input_word = 'brOwn';
-            $test_count = new Count($input_sentence, $input_word);
+            $test_count = new RepeatCounter($input_sentence, $input_word);
 
             $test_count->remove_special_chars();
             $test_count->remove_word_spaces();
@@ -80,7 +80,7 @@
         {
             $input_sentence = 'the qui<ck Br"own browN fox';
             $input_word = '.brOwn ';
-            $test_count = new Count($input_sentence, $input_word);
+            $test_count = new RepeatCounter($input_sentence, $input_word);
 
             $result = $test_count->count();
 
